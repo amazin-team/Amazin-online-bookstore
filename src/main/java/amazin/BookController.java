c4efb60329baf96f01802027830d339b19dfc0be
adnanhajar <adnanhajar@cmail.carleton.ca>
2019-03-06 14:09:41 -0500

    resources: _bookBrowser -- Added Jquery data table to our current table implementation

--- src/main/java/amazin/BookController.java
+++ src/main/java/amazin/BookController.java
@@ -1,7 +1,5 @@
 package amazin;
 
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,6 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class BookController {
@@ -32,7 +32,7 @@ public class BookController {
 
         repository.save(book);
         model.addAttribute("books", repository.findAll());
-        return "index";
+        return "redirect:/";
     }
 
     @GetMapping("/edit/{id}")
