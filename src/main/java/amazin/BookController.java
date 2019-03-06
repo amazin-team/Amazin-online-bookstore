b6edde81e325612dde7b18b90849c67c14cccad0
Zaidoon Abd Al Hadi <zaidoonabdalhadi@gmail.com>
2019-03-06 00:46:19 -0500

    added crud end points for book and updated book model

--- src/main/java/amazin/BookController.java
+++ src/main/java/amazin/BookController.java
@@ -1,32 +1,67 @@
 package amazin;
 
-        import org.springframework.beans.factory.annotation.Autowired;
-        import org.springframework.stereotype.Controller;
-        import org.springframework.ui.Model;
-        import org.springframework.web.bind.annotation.GetMapping;
-        import org.springframework.web.bind.annotation.ModelAttribute;
-        import org.springframework.web.bind.annotation.PostMapping;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class BookController {
 
-    //Autowire your CrudRepo of book
+    // Autowire your CrudRepo of book
     @Autowired
     BookRepository repository;
 
-    @GetMapping("/book")
+    @GetMapping("/addbook")
     public String bookForm(Model model) {
         model.addAttribute("book", new Book());
-        return "book";
+        return "create-book";
     }
 
-    @PostMapping("/book")
-    public String bookSubmit(@ModelAttribute Book book, Model model) {
-        System.out.println(book.toString());
-        Book new_Book = new Book();
+    @PostMapping("/addbook")
+    public String addBook(@Valid @ModelAttribute Book book, Model model, BindingResult result) {
+        if (result.hasErrors()) {
+            return "create-book";
+        }
+        Book new_Book = new Book(book.getName(), book.getDescription(), book.getISBN(), book.getPicture_path(),
+                book.getAuthor(), book.getPublisher(), book.getSku(), book.getInventory(), book.getPrice());
         repository.save(new_Book);
         model.addAttribute("new_book", new_Book);
         return "bookResult";
+    }
+
+    @GetMapping("/book/edit/{id}")
+    public String showUpdateForm(@PathVariable("id") Long id, Model model) {
+        Book book = repository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
+
+        model.addAttribute("book", book);
+        return "update-book";
+    }
+
+    @PostMapping("/book/update/{id}")
+    public String updateBook(@PathVariable("id") long id, @Valid Book book, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            book.setId(id);
+            return "update-user";
+        }
+
+        repository.save(book);
+        model.addAttribute("users", repository.findAll());
+        return "index";
+    }
+
+    @GetMapping("book/delete/{id}")
+    public String deleteBook(@PathVariable("id") Long id, Model model) {
+        Book book = repository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid book Id:" + id));
+        repository.delete(book);
+        model.addAttribute("books", repository.findAll());
+        return "index";
     }
 
 }
