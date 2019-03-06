b1be6dac88a041f1097501d5697f13d9bc7dc324
Laura Machado <lauramachado1021@gmail.com>
2019-03-06 11:47:09 -0500

    Fixed error.

--- src/main/java/amazin/User.java
+++ src/main/java/amazin/User.java
@@ -19,7 +19,7 @@ public class User {
     public User(Long id, String firstName, String lastName, String email, String password) {
         this.id = id;
         this.firstName = firstName;
-        this.lastName = lastName
+        this.lastName = lastName;
         this.email = email;
         this.password = password;
     }
