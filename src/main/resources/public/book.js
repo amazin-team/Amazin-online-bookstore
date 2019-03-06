130f2e411105b606d180ae4c8c10bd27c9e38d69
Aaron Bungay <amb240h@gmail.com>
2019-03-06 02:15:49 -0500

    Use JSON.stringify instead of .toString() to serialize Book objects
    When the user clicks on the "View list of books" button all that was being shown was "[Object Object]" which does not show any fields of the book to the user - not being of much use. Using JSON.stringify() instead for now we can see the different fields stored for this book.

--- src/main/resources/public/book.js
+++ src/main/resources/public/book.js
@@ -5,15 +5,14 @@ $(document).ready(function() {
         }).then(function (data) {
             $('#title').empty().append("Available books");
             let book = data._embedded.book;
-            if (book.length === 0 ){
+            if (book.length === 0 ) {
                 $('.content').empty().append("No books were found");
-            }
-            else{
-                let book_content= "";
-                for(let i =0 ; i < book.length ; i++){
-                    console.log(book[i]);
-                    book_content += `<br>${book[i].toString()}<br>`;
+            } else {
+                let book_content= "<ul>";
+                for (let i =0 ; i < book.length ; i++) {
+                    book_content += `<li><code>${JSON.stringify(book[i], null, 2)}</code></li>`;
                 }
+                book_content += "</ul>"
                 $('.content').empty().append(book_content);
             }
         });
@@ -23,7 +22,7 @@ $(document).ready(function() {
     $(".create_book").click(function() {
         $.ajax({
             url: "./book"
-        }).then(function(data){
+        }).then(function(data) {
             $('#title').empty();
             $('.content').empty().append(data);
         });
