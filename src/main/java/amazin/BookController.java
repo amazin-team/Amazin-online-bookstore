311bed3c5a3dca621f33e3befb704681b92959be
Adnan Hajar <adnanhajar@cmail.carleton.ca>
2019-03-10 22:06:47 -0400

    amazin: BookController: removed unnecessary import relocation

--- src/main/java/amazin/BookController.java
+++ src/main/java/amazin/BookController.java
@@ -1,5 +1,7 @@
 package amazin;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,8 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-
-import javax.validation.Valid;
 
 @Controller
 public class BookController {
