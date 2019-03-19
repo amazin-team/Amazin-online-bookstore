cfd9dbe6f136c389696dae5d31cfd4a3bd7ece22
Laura Machado <lauramachado1021@gmail.com>
2019-03-19 14:50:38 -0400

    Began building UI for shopping cart.

--- src/main/java/amazin/controller/ApplicationController.java
+++ src/main/java/amazin/controller/ApplicationController.java
@@ -1,11 +1,14 @@
 package amazin.controller;
 
+import amazin.model.ShoppingCart;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import amazin.repository.BookRepository;
+
+import java.util.Optional;
 
 @Controller
 public class ApplicationController {
