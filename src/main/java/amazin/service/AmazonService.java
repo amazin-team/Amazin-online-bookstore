06e2bfc7c9a4a6664709e69753a00956a00d2a24
Adnan Hajar <adhajar.work@gmail.com>
2019-03-20 10:38:20 -0400

    Feature(S3 storage): Add AmazonService

---
+++ src/main/java/amazin/service/AmazonService.java
@@ -0,0 +1,20 @@
+@Service
+public class AmazonClient {
+
+    private AmazonS3 s3client;
+
+    @Value("${amazonProperties.endpointUrl}")
+    private String endpointUrl;
+    @Value("${amazonProperties.bucketName}")
+    private String bucketName;
+    @Value("${amazonProperties.accessKey}")
+    private String accessKey;
+    @Value("${amazonProperties.secretKey}")
+    private String secretKey;
+
+    @PostConstruct
+    private void initializeAmazon() {
+        AWSCredentials credentials = new BasicAWSCredentials(this.accessKey, this.secretKey);
+        this.s3client = new AmazonS3Client(credentials);
+    }
+}
