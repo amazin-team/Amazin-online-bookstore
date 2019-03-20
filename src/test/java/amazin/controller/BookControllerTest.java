4e0bbb61c580dad06ee2b7828dc2f042d9308912
Zaidoon Abd Al Hadi <zaidoonabdalhadi@gmail.com>
2019-03-19 21:26:36 -0400

    refactor(BookControllerTest): moved some tests from BookControllerTest class to BookServiceTest class

--- src/test/java/amazin/controller/BookControllerTest.java
+++ src/test/java/amazin/controller/BookControllerTest.java
@@ -14,7 +14,6 @@ import java.util.Optional;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.validation.support.BindingAwareModelMap;
@@ -86,81 +85,5 @@ public class BookControllerTest {
         assertEquals(updated.getName(), formModel.getName());
         assertEquals(updated.getInventory(), formModel.getInventory());
         assertEquals(updated.getPrice(), formModel.getPrice(), 0.1);
-    }
-
-    @Test
-    public void createBook() {
-        Book formModel = BookTestUtil.createModel(BookTestUtil.ID, BookTestUtil.NAME, BookTestUtil.DESCRIPTION,
-                BookTestUtil.ISBN, BookTestUtil.PICTURE, BookTestUtil.AUTHOR, BookTestUtil.PUBLISHER,
-                BookTestUtil.INVENTORY, BookTestUtil.PRICE);
-
-        bookService.create(formModel);
-
-        ArgumentCaptor<Book> bookArgument = ArgumentCaptor.forClass(Book.class);
-        verify(repo, times(1)).save(bookArgument.capture());
-        verifyNoMoreInteractions(repo);
-
-        Book model = bookArgument.getValue();
-
-        assertNotNull(model.getId());
-        assertEquals(formModel.getDescription(), model.getDescription());
-        assertEquals(formModel.getAuthor(), model.getAuthor());
-        assertEquals(formModel.getPublisher(), model.getPublisher());
-        assertEquals(formModel.getISBN(), model.getISBN());
-        assertEquals(formModel.getName(), model.getName());
-        assertEquals(formModel.getInventory(), model.getInventory());
-        assertEquals(formModel.getPrice(), model.getPrice(), 0.1);
-    }
-
-    @Test
-    public void updateBook() {
-
-        Book formModel = BookTestUtil.createModel(BookTestUtil.ID, BookTestUtil.NAME_UPDATED,
-                BookTestUtil.DESCRIPTION_UPDATED, BookTestUtil.ISBN_UPDATED, BookTestUtil.PICTURE_UPDATED,
-                BookTestUtil.AUTHOR_UPDATED, BookTestUtil.PUBLISHER_UPDATED, BookTestUtil.INVENTORY_UPDATED,
-                BookTestUtil.PRICE_UPDATED);
-
-        Book model = BookTestUtil.createModel(BookTestUtil.ID, BookTestUtil.NAME, BookTestUtil.DESCRIPTION,
-                BookTestUtil.ISBN, BookTestUtil.PICTURE, BookTestUtil.AUTHOR, BookTestUtil.PUBLISHER,
-                BookTestUtil.INVENTORY, BookTestUtil.PRICE);
-
-        Optional<Book> optionalModel = Optional.of(model);
-        when(repo.findById(formModel.getId())).thenReturn(optionalModel);
-
-        Book actual = bookService.update(formModel);
-
-        verify(repo, times(1)).save(formModel);
-        verify(repo, times(1)).findById(formModel.getId());
-        verifyNoMoreInteractions(repo);
-
-        assertEquals(formModel.getDescription(), actual.getDescription());
-        assertEquals(formModel.getAuthor(), actual.getAuthor());
-        assertEquals(formModel.getPublisher(), actual.getPublisher());
-        assertEquals(formModel.getISBN(), actual.getISBN());
-        assertEquals(formModel.getName(), actual.getName());
-        assertEquals(formModel.getInventory(), actual.getInventory());
-        assertEquals(formModel.getPrice(), actual.getPrice(), 0.1);
-    }
-
-    @Test
-    public void deletBook() {
-        Book model = BookTestUtil.createModel(BookTestUtil.ID, BookTestUtil.NAME, BookTestUtil.DESCRIPTION,
-                BookTestUtil.ISBN, BookTestUtil.PICTURE, BookTestUtil.AUTHOR, BookTestUtil.PUBLISHER,
-                BookTestUtil.INVENTORY, BookTestUtil.PRICE);
-        Optional<Book> optionalModel = Optional.of(model);
-
-        when(repo.findById(BookTestUtil.ID)).thenReturn(optionalModel);
-
-        Optional<Book> optionalActual = bookService.delete(BookTestUtil.ID);
-
-        assertTrue("Book must be present", optionalActual.isPresent());
-
-        Book actual = optionalActual.get();
-
-        verify(repo, times(1)).findById(BookTestUtil.ID);
-        verify(repo, times(1)).delete(model);
-        verifyNoMoreInteractions(repo);
-
-        assertEquals(model, actual);
     }
 }
--- src/test/java/amazin/service/BookServiceTest.java
+++ src/test/java/amazin/service/BookServiceTest.java
@@ -1,17 +1,24 @@
 package amazin.service;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 
 import java.util.Optional;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import amazin.BookTestUtil;
 import amazin.model.Book;
 import amazin.repository.BookRepository;
 
@@ -44,27 +51,77 @@ public class BookServiceTest {
         verify(repository).save(book);
     }
 
+    public void createTest() {
+        Book formModel = BookTestUtil.createModel(BookTestUtil.ID, BookTestUtil.NAME, BookTestUtil.DESCRIPTION,
+                BookTestUtil.ISBN, BookTestUtil.PICTURE, BookTestUtil.AUTHOR, BookTestUtil.PUBLISHER,
+                BookTestUtil.INVENTORY, BookTestUtil.PRICE);
+
+        service.create(formModel);
+
+        ArgumentCaptor<Book> bookArgument = ArgumentCaptor.forClass(Book.class);
+        verify(repository, times(1)).save(bookArgument.capture());
+        verifyNoMoreInteractions(repository);
+
+        Book model = bookArgument.getValue();
+
+        assertNotNull(model.getId());
+        assertEquals(formModel.getDescription(), model.getDescription());
+        assertEquals(formModel.getAuthor(), model.getAuthor());
+        assertEquals(formModel.getPublisher(), model.getPublisher());
+        assertEquals(formModel.getISBN(), model.getISBN());
+        assertEquals(formModel.getName(), model.getName());
+        assertEquals(formModel.getInventory(), model.getInventory());
+        assertEquals(formModel.getPrice(), model.getPrice(), 0.1);
+    }
+
     @Test
     public void updateTest() {
-        Book book = mock(Book.class);
-        Optional<Book> optionalBook = Optional.of(book);
-        when(book.getId()).thenReturn(ID);
-        when(book.getAuthor()).thenReturn("Test Author");
-        when(book.getName()).thenReturn("Test Name");
-        when(book.getDescription()).thenReturn("Test Description");
-        when(book.getISBN()).thenReturn("1111111111111");
-        when(book.getInventory()).thenReturn(12);
-        when(book.getPicture()).thenReturn("Test Picture");
-        when(book.getPrice()).thenReturn(56.34);
-        when(book.getPublisher()).thenReturn("Test Publisher");
-        when(repository.findById(ID)).thenReturn(optionalBook);
-        service.update(book);
-        verify(repository).save(book);
+        Book formModel = BookTestUtil.createModel(BookTestUtil.ID, BookTestUtil.NAME_UPDATED,
+                BookTestUtil.DESCRIPTION_UPDATED, BookTestUtil.ISBN_UPDATED, BookTestUtil.PICTURE_UPDATED,
+                BookTestUtil.AUTHOR_UPDATED, BookTestUtil.PUBLISHER_UPDATED, BookTestUtil.INVENTORY_UPDATED,
+                BookTestUtil.PRICE_UPDATED);
+
+        Book model = BookTestUtil.createModel(BookTestUtil.ID, BookTestUtil.NAME, BookTestUtil.DESCRIPTION,
+                BookTestUtil.ISBN, BookTestUtil.PICTURE, BookTestUtil.AUTHOR, BookTestUtil.PUBLISHER,
+                BookTestUtil.INVENTORY, BookTestUtil.PRICE);
+
+        Optional<Book> optionalModel = Optional.of(model);
+        when(repository.findById(formModel.getId())).thenReturn(optionalModel);
+
+        Book actual = service.update(formModel);
+
+        verify(repository, times(1)).save(formModel);
+        verify(repository, times(1)).findById(formModel.getId());
+        verifyNoMoreInteractions(repository);
+
+        assertEquals(formModel.getDescription(), actual.getDescription());
+        assertEquals(formModel.getAuthor(), actual.getAuthor());
+        assertEquals(formModel.getPublisher(), actual.getPublisher());
+        assertEquals(formModel.getISBN(), actual.getISBN());
+        assertEquals(formModel.getName(), actual.getName());
+        assertEquals(formModel.getInventory(), actual.getInventory());
+        assertEquals(formModel.getPrice(), actual.getPrice(), 0.1);
     }
 
     @Test
     public void deleteTest() {
-        service.delete(ID);
-        verify(repository).deleteById(ID);
+        Book model = BookTestUtil.createModel(BookTestUtil.ID, BookTestUtil.NAME, BookTestUtil.DESCRIPTION,
+                BookTestUtil.ISBN, BookTestUtil.PICTURE, BookTestUtil.AUTHOR, BookTestUtil.PUBLISHER,
+                BookTestUtil.INVENTORY, BookTestUtil.PRICE);
+        Optional<Book> optionalModel = Optional.of(model);
+
+        when(repository.findById(BookTestUtil.ID)).thenReturn(optionalModel);
+
+        Optional<Book> optionalActual = service.delete(BookTestUtil.ID);
+
+        assertTrue("Book must be present", optionalActual.isPresent());
+
+        Book actual = optionalActual.get();
+
+        verify(repository, times(1)).findById(BookTestUtil.ID);
+        verify(repository, times(1)).delete(model);
+        verifyNoMoreInteractions(repository);
+
+        assertEquals(model, actual);
     }
 }
