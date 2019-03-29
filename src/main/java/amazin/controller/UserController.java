4eb3eeec2e7649c385498b4591660a9ed73d7481
Aaron Bungay <amb240h@gmail.com>
2019-03-29 18:56:23 -0400

    refactor: add constants for comonly used views in UserController

--- src/main/java/amazin/controller/UserController.java
+++ src/main/java/amazin/controller/UserController.java
@@ -25,62 +25,80 @@ public class UserController {
     @Autowired
     private UserValidator userValidator;
 
+    public static final String VIEW_LOGIN = "login";
+    public static final String VIEW_REGISTER = "registration";
+    public static final String VIEW_REGISTER_ADMIN = "registration-admin";
+    public static final String VIEW_FORGOT_PASSWORD = "forgot-password";
+
+    public static final String REQUEST_MAPPING_INDEX = "/";
+
+    public static final String MODEL_ATTRIBUTE_USER = "user";
+    public static final String MODEL_ATTRIBUTE_ERROR = "error";
+    public static final String MODEL_ATTRIBUTE_MESSAGE = "message";
+
+    public static final String ERROR_LOGIN_INVALID = "Invalid username or password.";
+    public static final String MESSAGE_LOGOUT_SUCCESS = "Successfully logged out.";
+
     private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @GetMapping("/login")
     public String login(Model model, String error, String logout) {
         if (error != null)
-            model.addAttribute("error", "Invalid username or password.");
+            model.addAttribute(MODEL_ATTRIBUTE_ERROR, ERROR_LOGIN_INVALID);
 
         if (logout != null)
-            model.addAttribute("message", "Successfully logged out.");
+            model.addAttribute(MODEL_ATTRIBUTE_MESSAGE, MESSAGE_LOGOUT_SUCCESS);
 
-        return "login";
+        return VIEW_LOGIN;
     }
 
     @GetMapping("/register")
     public String register(Model model){
-        model.addAttribute("user", new User());
+        model.addAttribute(MODEL_ATTRIBUTE_USER, new User());
 
-        return "registration";
+        return VIEW_REGISTER;
     }
 
     @PostMapping("/register")
     public String register(@Valid @ModelAttribute User userForm, BindingResult result, Model model) {
         userValidator.validate(userForm, result);
         if (result.hasErrors()) {
-            return "registration";
+            return VIEW_REGISTER;
         }
 
         userService.save(userForm);
         securityService.autoLogin(userForm.getEmail(), userForm.getPasswordConfirmation());
 
-        return "redirect:/";
+        return getRedirectViewPath(REQUEST_MAPPING_INDEX);
     }
 
     @GetMapping("/admin/register")
     public String registerAdmin(Model model){
-        model.addAttribute("user", new User());
+        model.addAttribute(MODEL_ATTRIBUTE_USER, new User());
 
-        return "registration-admin";
+        return VIEW_REGISTER_ADMIN;
     }
 
     @PostMapping("/admin/register")
     public String registerAdmin(@Valid @ModelAttribute User userForm, BindingResult result, Model model) {
         /** TODO has extra token as opposed to normal register and needs to set ROLE_ADMIN role in addition to ROLE_USER */
 
-        return "redirect:/";
+        return getRedirectViewPath(REQUEST_MAPPING_INDEX);
     }
 
     @GetMapping("/forgot-password")
     public String forgotPassword(Model model){
-        return "forgot-password";
+        return VIEW_FORGOT_PASSWORD;
     }
 
     @PostMapping("/forgot-password")
     public String forgotPassword(@Valid @ModelAttribute User userForm, BindingResult result, Model model) {
         /** TODO */
 
-        return "redirect:/";
+        return getRedirectViewPath(REQUEST_MAPPING_INDEX);
+    }
+
+    private String getRedirectViewPath(String requestMapping) {
+        return "redirect:" + requestMapping;
     }
 }
