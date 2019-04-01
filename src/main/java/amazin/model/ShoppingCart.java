d1fa2e64403dba9d2533c50d6a2325b379bfb306
Laura Machado <lauramachado1021@gmail.com>
2019-04-01 16:23:40 -0400

    Minor change.

--- src/main/java/amazin/model/ShoppingCart.java
+++ src/main/java/amazin/model/ShoppingCart.java
@@ -17,17 +17,33 @@ import java.util.Map;
 public class ShoppingCart {
 
     private ArrayList<Item> items;
+    private int itemCount;
 
     public ShoppingCart() {
+
         this.items = new ArrayList<>();
+        itemCount = 0;
     }
 
     public ArrayList<Item> getItems() {
         return this.items;
     }
 
+    public int getItemCount(){
+        return this.itemCount;
+    }
+
     public void addItem(Item item){
         this.items.add(item);
+    }
+
+    public void updateItemCount(){
+        int count = 0;
+        for(Item item : items){
+            count+=item.getQuantity();
+        }
+
+        this.itemCount = count;
     }
 
 }
