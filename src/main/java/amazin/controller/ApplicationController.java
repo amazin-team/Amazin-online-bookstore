ca17a5e496402c9318e379e03a2d268cd8a7b369
abungay <amb240h@gmail.com>
2019-04-03 03:36:39 -0400

    feat: add current user model to GET index for ApplicationController

--- src/main/java/amazin/controller/ApplicationController.java
+++ src/main/java/amazin/controller/ApplicationController.java
@@ -1,5 +1,8 @@
 package amazin.controller;
 
+import amazin.model.User;
+import amazin.repository.UserRepository;
+import amazin.service.SecurityServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,10 +15,18 @@ public class ApplicationController {
 
     @Autowired
     BookRepository bookRepository;
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    SecurityServiceImpl securityService;
 
     @GetMapping("/")
     public String index(Model model) {
+        User currentUser = userRepository.findByEmail(securityService.findLoggedInEmail());
+
         model.addAttribute(BookController.MODEL_ATTRIBUTE_BOOK, bookRepository.findAll());
+        model.addAttribute(UserController.MODEL_ATTRIBUTE_USER, currentUser);
+
         return "index";
     }
 }
