38bfe39eb64f08cda4f5eefb1ec268df5af0124b
Laura Machado <lauramachado1021@gmail.com>
2019-04-03 01:31:30 -0400

    Added a few model tests.

--- src/main/java/amazin/controller/ShoppingCartController.java
+++ src/main/java/amazin/controller/ShoppingCartController.java
@@ -8,7 +8,6 @@ import amazin.repository.BookRepository;
 import amazin.repository.ShoppingCartRepository;
 import amazin.repository.UserRepository;
 import amazin.service.ShoppingCartService;
-import org.jboss.jdeparser.FormatPreferences;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -16,12 +15,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-import javax.validation.Valid;
-import java.lang.reflect.Array;
-import java.security.Principal;
 import java.util.*;
 
 /**
